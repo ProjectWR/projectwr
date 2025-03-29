0082bfdb181c3eaa2e0718ad0a9b16f3a9af229e
Yeshan Kaushik <yeshankaushikyk@gmail.com>
2025-03-29 15:48:17 +0530

    added clipboard api

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -20,6 +20,7 @@ fn fetch_default_settings() -> Result<Settings, String> {
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
+        .plugin(tauri_plugin_clipboard_manager::init())
         .plugin(tauri_plugin_store::Builder::default().build())
         .plugin(tauri_plugin_fs::init())
         .plugin(tauri_plugin_dialog::init())
