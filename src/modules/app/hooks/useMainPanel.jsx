cd2db3bb3637e5f9bb2bff0ac594f238746dc463
Yeshan Kaushik <yeshankaushikyk@gmail.com>
2025-05-11 03:44:40 +0530

    setting up tabs

--- src/modules/app/hooks/useMainPanel.jsx
+++ src/modules/app/hooks/useMainPanel.jsx
@@ -1,11 +1,22 @@
 import { useCallback } from "react";
 import { mainPanelStore } from "../stores/mainPanelStore";
 import useStoreHistory from "./useStoreHistory";
+import { equalityDeep } from "lib0/function";
 
 const useMainPanel = () => {
+  /**
+   * @type {MainPanelState}
+   * @typedef {Object} MainPanelState
+   * @property {string} panelType - The current panel type (e.g., "home").
+   * @property {*} mode - The current mode of the panel (can be null or specific mode).
+   * @property {Array} breadcrumbs - An array of breadcrumb strings representing the navigation path.
+   */
   const mainPanelState = mainPanelStore((state) => state.mainPanelState);
   const setMainPanelState = mainPanelStore((state) => state.setMainPanelState);
 
+  /**
+   * @type {Array<MainPanelState>}
+   */
   const tabs = mainPanelStore((state) => state.tabs);
 
   const arr = [];
@@ -30,7 +41,16 @@ const useMainPanel = () => {
         breadcrumbs: breadcrumbs,
       };
 
-      // if (arr.find())
+      if (
+        !tabs?.find((value) => {
+          return equalityDeep(value, newState);
+        })
+      ) {
+        tabs.push(newState);
+        if (tabs.size() > 10) {
+          tabs.shift();
+        }
+      }
 
       setMainPanelState(newState);
 
