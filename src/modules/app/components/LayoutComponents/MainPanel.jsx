32544aaf5dc1693d1b06557b41acd43a41c4d2cf
Yeshan Kaushik <yeshankaushikyk@gmail.com>
2025-05-30 01:21:38 +0530

    main panel animations

--- src/modules/app/components/LayoutComponents/MainPanel.jsx
+++ src/modules/app/components/LayoutComponents/MainPanel.jsx
@@ -466,9 +466,9 @@ const MainPanel = ({ isNotesPanelAwake, refreshNotesPanel }) => {
       <AnimatePresence mode="wait">
         <motion.div
           key={key.current}
-          initial={{ y: 15, opacity: 0 }}
+          initial={{ y: -5, opacity: 0 }}
           animate={{ y: 0, opacity: 1 }}
-          exit={{ y: 15, opacity: 0 }}
+          exit={{ y: 5, opacity: 0 }}
           transition={{ duration: 0.1 }}
           className="w-full grow min-h-0 basis-0 overflow-hidden z-3 flex flex-col items-center justify-center"
         >
