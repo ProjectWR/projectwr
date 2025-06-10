d5300286338aa4ba684e30717dd6f057e2eaf11f
Yeshan Kaushik <yeshankaushikyk@gmail.com>
2025-06-10 11:20:44 +0530

    made text editor small

--- src/modules/app/components/LayoutComponents/DetailsPanel/DetailsPanelProps.jsx
+++ src/modules/app/components/LayoutComponents/DetailsPanel/DetailsPanelProps.jsx
@@ -231,10 +231,11 @@ export const DetailsPanelDescriptionProp = ({
               root: "bg-appBackground border border-appLayoutBorder rounded-lg px-1",
               toolbar: "bg-appBackground border-b border-appLayoutBorder",
               content:
-                "bg-appBackground text-appLayoutText  max-h-[30rem] min-h-[10rem] overflow-y-scroll px-3 py-3 text-detailsPanelPropFontSize DetailsPanelDescriptionProp",
+                "bg-appBackground text-appLayoutText  max-h-[30rem] min-h-fit overflow-y-scroll px-3 py-3 text-detailsPanelPropFontSize DetailsPanelDescriptionProp",
               controlsGroup: "bg-appBackground gap-1",
               control:
                 "bg-appBackground border-none border-appLayoutBorder text-appLayoutText overflow-hidden hover:bg-appLayoutInverseHover hover:text-appLayoutText  data-active:bg-appLayoutPressed data-active:shadow-inner shadow-appLayoutShadow",
+
             }}
           >
             <style>
