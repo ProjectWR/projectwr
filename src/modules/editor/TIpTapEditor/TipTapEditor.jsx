39c02c7d7ca911a3662123fe15dc24486ca60d15
Yeshan Kaushik <yeshankaushikyk@gmail.com>
2025-06-23 21:59:33 +0530

    top panel

--- src/modules/editor/TIpTapEditor/TipTapEditor.jsx
+++ src/modules/editor/TIpTapEditor/TipTapEditor.jsx
@@ -700,6 +700,13 @@ const TiptapEditor = ({
         </style>
 
         <div
+          id="EditorTopPanelsContainer"
+          className="absolute top-0 left-0 -translate-y-full w-full z-[3] h-[20rem] flex gap-1 justify-center items-center"
+        >
+          
+        </div>
+
+        <div
           id="EditorSidePanelsContainer"
           className="absolute top-0 right-0 translate-x-full w-[20rem] z-[3] h-full flex flex-col gap-1 justify-center items-center"
         >
